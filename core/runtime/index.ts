b437b674b639ad2268b7b05cfc92c15b00abb46c
SA SUJON <111200248+SA-SUJON@users.noreply.github.com>
2026-09-15 00:32:30 +0600

    feat(mark05): export task reasoning engine

--- core/runtime/index.ts
+++ core/runtime/index.ts
@@ -3,3 +3,4 @@ export * from "./TaskContext.js";
 export * from "./ExecutionStateMachine.js";
 export * from "./RecoveryEngine.js";
 export * from "./PlanValidationEngine.js";
+export * from "./TaskReasoningEngine.js";
