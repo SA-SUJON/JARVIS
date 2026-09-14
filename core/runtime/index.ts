000ee965afcb2a4f281b626a7e53f7f15797083a
SA SUJON <111200248+SA-SUJON@users.noreply.github.com>
2026-09-15 00:28:12 +0600

    feat(mark05): export plan validation engine

--- core/runtime/index.ts
+++ core/runtime/index.ts
@@ -2,3 +2,4 @@ export * from "./RuntimeKernel.js";
 export * from "./TaskContext.js";
 export * from "./ExecutionStateMachine.js";
 export * from "./RecoveryEngine.js";
+export * from "./PlanValidationEngine.js";
